c66ad174bfca59ab8001f29b6f57b59d8db61ba8
dannilo <dannilo-pires@hotmail.com>
2022-08-13 08:29:45 -0300

    cria model book e service product-list

---
+++ src/app/components/bookstore-app/product-list/product-list.component.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpHeaders } from "@angular/common/http"
+import { Book } from "./model/Book";
+
+@Injectable()
+
+export class BookService {
+
+    
+
+}
